7602748bbb6479e91658ae87e9ae2385c8a92d9c
Miguelul <mguelantonio00@gmail.com>
2023-08-26 15:03:21 -0400

    first commit

---
+++ ProyectoFinalLab4/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ProyectoFinalLab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.bunifuRange1 = new Bunifu.Framework.UI.BunifuRange();
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(165, 112);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(75, 23);
+            this.iconButton1.TabIndex = 0;
+            this.iconButton1.Text = "iconButton1";
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // bunifuRange1
+            // 
+            this.bunifuRange1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuRange1.BackgroudColor = System.Drawing.Color.DarkGray;
+            this.bunifuRange1.BorderRadius = 0;
+            this.bunifuRange1.IndicatorColor = System.Drawing.Color.SeaGreen;
+            this.bunifuRange1.Location = new System.Drawing.Point(234, 186);
+            this.bunifuRange1.MaximumRange = 100;
+            this.bunifuRange1.Name = "bunifuRange1";
+            this.bunifuRange1.RangeMax = 49;
+            this.bunifuRange1.RangeMin = 0;
+            this.bunifuRange1.Size = new System.Drawing.Size(415, 28);
+            this.bunifuRange1.TabIndex = 1;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = null;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bunifuRange1);
+            this.Controls.Add(this.iconButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Bunifu.Framework.UI.BunifuRange bunifuRange1;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+    }
+}
